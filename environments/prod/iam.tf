a7f942e6387f2ac4eeb5890d22b8f66255b5b45e
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 17:56:59 -0600

    little bit of clean up, getting the bucket state right in terraform

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,8 +1,3 @@
-#resource "google_project_iam_policy" "iam_access_project_policy" {
-#  project     = var.project
-#  policy_data = data.google_iam_policy.iam_access.policy_data
-#}
-
 resource "google_project_iam_binding" "storage_object_access" {
   project = var.project
   role    = "roles/storage.objectViewer"
@@ -11,8 +6,3 @@ resource "google_project_iam_binding" "storage_object_access" {
     "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
   ]
 }
-
-#resource "google_project_iam_policy" "iam_access_project_policy" {
-#  project     = var.project
-#  policy_data = data.google_iam_policy.iam_access.policy_data
-#}
