4933ef22f581bf6186142ba2e2287f77a708cf36
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 12:59:03 -0600

    added required properties for bucket creation

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,24 +1,24 @@
-resource "google_project_iam_policy" "project" {
-  project     = var.project
-  policy_data = data.google_iam_policy.editor_access.policy_data
-}
-
-data "google_iam_policy" "editor_access" {
-  binding {
-    role = "roles/editor"
-
-    members = [
-      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-    ]
-  }
-}
-
-data "google_iam_policy" "storage_object_access" {
-  binding {
-    role = "roles/storage.objectViewer"
-
-    members = [
-      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-    ]
-  }
-}
+#resource "google_project_iam_policy" "project" {
+#  project     = var.project
+#  policy_data = data.google_iam_policy.editor_access.policy_data
+#}
+#
+#data "google_iam_policy" "editor_access" {
+#  binding {
+#    role = "roles/editor"
+#
+#    members = [
+#      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
+#    ]
+#  }
+#}
+#
+#data "google_iam_policy" "storage_object_access" {
+#  binding {
+#    role = "roles/storage.objectViewer"
+#
+#    members = [
+#      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
+#    ]
+#  }
+#}
