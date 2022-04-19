87185258d7d625a63e974477988db6b8c870664a
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 17:45:55 -0600

    object viewer for backend creation

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -3,42 +3,16 @@
 #  policy_data = data.google_iam_policy.iam_access.policy_data
 #}
 
-resource "google_project_iam_policy" "editor_access_project_policy" {
-  project     = var.project
-  policy_data = data.google_iam_policy.editor_access.policy_data
+resource "google_project_iam_binding" "project" {
+  project = var.project
+  role    = "roles/storage.objectViewer"
+
+  members = [
+    "user:343129465024@cloudbuild.gserviceaccount.com",
+  ]
 }
 
-resource "google_project_iam_policy" "storage_object_access_project_policy" {
-  project     = var.project
-  policy_data = data.google_iam_policy.storage_object_access.policy_data
-}
-
-#data "google_iam_policy" "iam_access" { //necessary for setting IAM from cloud build user
-#  binding {
-#    role = "roles/resourcemanager.projectIamAdmin"
-#
-#    members = [
-#      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-#    ]
-#  }
+#resource "google_project_iam_policy" "iam_access_project_policy" {
+#  project     = var.project
+#  policy_data = data.google_iam_policy.iam_access.policy_data
 #}
-
-data "google_iam_policy" "editor_access" {
-  binding {
-    role = "roles/editor"
-
-    members = [
-      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-    ]
-  }
-}
-
-data "google_iam_policy" "storage_object_access" {
-  binding {
-    role = "roles/storage.objectViewer"
-
-    members = [
-      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-    ]
-  }
-}
