0a92559786ba6707bdc21eb141ed21a23e58dafb
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 16:47:20 -0600

    getting rid of the problem iam for now

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,29 +1,27 @@
-resource "google_project_iam_policy" "iam_access_project_policy" {
-  project     = var.project
-  policy_data = data.google_iam_policy.iam_access.policy_data
-}
+#resource "google_project_iam_policy" "iam_access_project_policy" {
+#  project     = var.project
+#  policy_data = data.google_iam_policy.iam_access.policy_data
+#}
 
 resource "google_project_iam_policy" "editor_access_project_policy" {
   project     = var.project
   policy_data = data.google_iam_policy.editor_access.policy_data
-  depends_on = [google_project_iam_policy.iam_access_project_policy]
 }
 
 resource "google_project_iam_policy" "storage_object_access_project_policy" {
   project     = var.project
   policy_data = data.google_iam_policy.storage_object_access.policy_data
-  depends_on = [google_project_iam_policy.iam_access_project_policy]
 }
 
-data "google_iam_policy" "iam_access" { //necessary for setting IAM from cloud build user
-  binding {
-    role = "roles/resourcemanager.projectIamAdmin"
-
-    members = [
-      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-    ]
-  }
-}
+#data "google_iam_policy" "iam_access" { //necessary for setting IAM from cloud build user
+#  binding {
+#    role = "roles/resourcemanager.projectIamAdmin"
+#
+#    members = [
+#      "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
+#    ]
+#  }
+#}
 
 data "google_iam_policy" "editor_access" {
   binding {
