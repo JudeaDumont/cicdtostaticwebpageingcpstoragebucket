e6adeb492d0db8820daa34084ca7091377c9b496
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 21:20:13 -0600

    Need permission to change it

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -9,9 +9,10 @@ resource "google_storage_bucket_iam_binding" "binding" {
 
 resource "google_storage_bucket_iam_binding" "cloud_build_access_to_iam" {
   bucket = google_storage_bucket.web_bucket.name
-  role    = "roles/storage.legacyBucketOwner"
+  role    = "roles/storage.legacyBucketOwner" //this needs to be refactored to have only the permissions it needs
 
   members = [
     "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
+    "user:dumontjudea3@gmail.com"
   ]
-}#
+}
