f1bf84a25ef68480eab9c663ed10f3119f002acd
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 20:24:57 -0600

    Admin is required

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,6 +1,6 @@
 resource "google_project_iam_binding" "storage_object_access" {
   project = var.project
-  role    = "roles/storage.objectViewer"
+  role    = "roles/storage.admin"
 
   members = [
     "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
