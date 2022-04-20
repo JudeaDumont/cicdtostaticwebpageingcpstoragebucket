884e1d12baccc9e5298221bd55549f2c955274b1
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 20:50:08 -0600

    all users can view the website

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,6 +1,7 @@
-data "google_iam_policy" "all_users_can_view_website" {
-  binding {
-    role    = "roles/storage.objectViewer"
-    members = ["allUsers"]
-  }
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = google_storage_bucket.web_bucket
+  role = "roles/storage.objectViewer"
+  members = [
+    "allUsers",
+  ]
 }
