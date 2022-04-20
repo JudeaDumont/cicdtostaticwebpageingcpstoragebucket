e0943b9c0ff713ccfb6ca9cb6fa2af65037ac73f
Judea Dumont <dumontjudea2@gmail.com>
2022-04-19 20:22:18 -0600

    Adding a template site and the final steps to push into the bucket

--- environments/prod/iam.tf
+++ environments/prod/iam.tf
@@ -1,8 +1,8 @@
-#resource "google_project_iam_binding" "storage_object_access" {
-#  project = var.project
-#  role    = "roles/storage.objectViewer"
-#
-#  members = [
-#    "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
-#  ]
-#}
+resource "google_project_iam_binding" "storage_object_access" {
+  project = var.project
+  role    = "roles/storage.objectViewer"
+
+  members = [
+    "serviceAccount:343129465024@cloudbuild.gserviceaccount.com",
+  ]
+}
